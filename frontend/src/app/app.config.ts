014abf36a6861fc6092385632500a1ece76b6c86
Ajay <nkajayshankar@gmail.com>
2026-05-06 18:14:34 +0530

    fix: use HashLocationStrategy to resolve 404 errors on GitHub Pages

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
@@ -9,7 +9,7 @@ import { authInterceptor } from '../services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideToastr({
