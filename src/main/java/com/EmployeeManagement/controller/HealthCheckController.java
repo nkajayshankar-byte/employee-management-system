109513edf83286055f0385cfb83f1bf45d3047bb
Ajay <nkajayshankar@gmail.com>
2026-05-06 16:05:20 +0530

    feat: add root health check endpoint for Render

---
+++ src/main/java/com/EmployeeManagement/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.EmployeeManagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Employee Management System API is running!";
+    }
+}
